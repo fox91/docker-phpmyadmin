cfa5a20ed68778e60ba747840347990b1d1c5dfe
Andrea Falco <fox91fox@gmail.com>
2016-03-03 14:57:26 +0100

    phpMyAdmin 4.5.5.1

--- build.sh
+++ build.sh
@@ -5,23 +5,23 @@ set -x
 
 docker pull fox91/phpmyadmin:latest
 # docker build -t fox91/phpmyadmin:latest ./
-docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5.5-fpm
+docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5.5.1-fpm
 docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5-fpm
 docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:fpm
-docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5.5
+docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5.5.1
 docker tag fox91/phpmyadmin:latest docker.io/fox91/phpmyadmin:4.5
 
 docker pull fox91/phpmyadmin:4.5-apache
 # docker build -t fox91/phpmyadmin:4.5-apache ./4.5-apache
-docker tag fox91/phpmyadmin:4.5-apache docker.io/fox91/phpmyadmin:4.5.5-apache
+docker tag fox91/phpmyadmin:4.5-apache docker.io/fox91/phpmyadmin:4.5.5.1-apache
 docker tag fox91/phpmyadmin:4.5-apache docker.io/fox91/phpmyadmin:apache
 
 docker pull fox91/phpmyadmin:4.4-fpm
 # docker build -t fox91/phpmyadmin:4.4-fpm ./4.4-fpm
-docker tag fox91/phpmyadmin:4.4-fpm docker.io/fox91/phpmyadmin:4.4.15.4-fpm
-docker tag fox91/phpmyadmin:4.4-fpm docker.io/fox91/phpmyadmin:4.4.15.4
+docker tag fox91/phpmyadmin:4.4-fpm docker.io/fox91/phpmyadmin:4.4.15.5-fpm
+docker tag fox91/phpmyadmin:4.4-fpm docker.io/fox91/phpmyadmin:4.4.15.5
 docker tag fox91/phpmyadmin:4.4-fpm docker.io/fox91/phpmyadmin:4.4
 
 docker pull fox91/phpmyadmin:4.4-apache
 # docker build -t fox91/phpmyadmin:4.4-apache ./4.4-apache
-docker tag fox91/phpmyadmin:4.4-apache docker.io/fox91/phpmyadmin:4.4.15.4-apache
+docker tag fox91/phpmyadmin:4.4-apache docker.io/fox91/phpmyadmin:4.4.15.5-apache
--- clean.sh
+++ clean.sh
@@ -3,17 +3,17 @@ set -e
 set -u
 set -x
 
-docker rmi fox91/phpmyadmin:4.5.5-fpm
+docker rmi fox91/phpmyadmin:4.5.5.1-fpm
 docker rmi fox91/phpmyadmin:4.5-fpm
 docker rmi fox91/phpmyadmin:fpm
-docker rmi fox91/phpmyadmin:4.5.5
+docker rmi fox91/phpmyadmin:4.5.5.1
 docker rmi fox91/phpmyadmin:4.5
 
-docker rmi fox91/phpmyadmin:4.5.5-apache
+docker rmi fox91/phpmyadmin:4.5.5.1-apache
 docker rmi fox91/phpmyadmin:apache
 
-docker rmi fox91/phpmyadmin:4.4.15.4-fpm
-docker rmi fox91/phpmyadmin:4.4.15.4
+docker rmi fox91/phpmyadmin:4.4.15.5-fpm
+docker rmi fox91/phpmyadmin:4.4.15.5
 docker rmi fox91/phpmyadmin:4.4
 
-docker rmi fox91/phpmyadmin:4.4.15.4-apache
+docker rmi fox91/phpmyadmin:4.4.15.5-apache
