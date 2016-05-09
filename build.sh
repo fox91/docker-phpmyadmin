f22216190cff0b44229474321085996162b31690
Andrea Falco <fox91fox@gmail.com>
2016-05-09 14:20:53 +0200

    Bugfix

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
-set -u
+set -eu
 set -x
 
 # docker build -t fox91/phpmyadmin:latest ./
@@ -12,20 +11,20 @@ docker tag "${IMAGE_ID}" fox91/phpmyadmin:fpm
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.6.0
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.6
 
-# docker build -t fox91/phpmyadmin:4.6-apache ./
+# docker build -t fox91/phpmyadmin:4.6-apache ./4.6-apache/
 docker pull fox91/phpmyadmin:4.6-apache
 IMAGE_ID=$(docker images -q fox91/phpmyadmin:4.6-apache)
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.6.0-apache
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:apache
 
-# docker build -t fox91/phpmyadmin:4.4-fpm ./
+# docker build -t fox91/phpmyadmin:4.4-fpm ./4.4-fpm/
 docker pull fox91/phpmyadmin:4.4-fpm
 IMAGE_ID=$(docker images -q fox91/phpmyadmin:4.4-fpm)
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.4.15.5-fpm
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.4.15.5
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.4
 
-# docker build -t fox91/phpmyadmin:4.4-apache ./
+# docker build -t fox91/phpmyadmin:4.4-apache ./4.4-apache/
 docker pull fox91/phpmyadmin:4.4-apache
 IMAGE_ID=$(docker images -q fox91/phpmyadmin:4.4-apache)
 docker tag "${IMAGE_ID}" fox91/phpmyadmin:4.4.15.5-apache
